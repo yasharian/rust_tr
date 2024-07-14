63dd8c57bc72248c7f1f24658a636cd874094aa8
yahsar <yasharian1383@gmail.com>
2024-07-15 00:08:14 +0330

    again me

--- src/this.rs
+++ src/this.rs
@@ -3,7 +3,9 @@ use std::io;
 fn main(){
     let mut string = io::String();
     io::stdin().read_line(&mut string).expect("the statement was unreadable");
-	let mut this = 5;
+	
+    let mut this = 5;
+    println!("{}" , this );
 	this = 4 ; 
 	this = 3;
 	println!("{this}");
